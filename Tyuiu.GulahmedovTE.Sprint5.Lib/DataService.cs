a847b9d82d609333bd354cf73024712a5f7be95e
Tyuiu.GulahmedovTG@outlook.com <Tyuiu.GulahmedovTG@outlook.com>
2024-11-26 01:54:13 +0500

    task0

--- Tyuiu.GulahmedovTE.Sprint5.Lib/DataService.cs
+++ Tyuiu.GulahmedovTE.Sprint5.Lib/DataService.cs
@@ -8,6 +8,7 @@ namespace Tyuiu.GulahmedovTE.Sprint5.Lib
         {
             string[] paths = { $@"{Path.GetTempPath()}", "OutPutFileTask0.txt" };
             string fullPath = Path.Combine(paths);
+
             double z = Math.Round(0.7 * Math.Pow(x, 3) + 1.52 * Math.Pow(x, 2), 3);
             File.WriteAllText(fullPath, z.ToString());
             return fullPath;
--- Tyuiu.GulahmedovTE.Sprint5/Program.cs
+++ Tyuiu.GulahmedovTE.Sprint5/Program.cs
@@ -10,13 +10,11 @@ namespace Tyuiu.GulahmedovTE.Sprint5
             int x = 2;
 
             Console.WriteLine($"x = {2}");
-
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
 
             string res = ds.SaveToFileTextData(x);
-
             Console.WriteLine($"Файл: {res}");
             Console.WriteLine("Создан");
             Console.ReadKey();
